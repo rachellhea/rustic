b13ce71e6be978cc0c038829be17fb1295c43fd0
Rachel Forshee <rachel.i.forshee@gmail.com>
2020-05-07 09:06:27 -0700

    Implement remaining iNES file header parsing.
    New fields:
    * Trainer existence flag
    * Mapper number
    * TV type (NTSC, PAL, dual-compatible)
    * PRG RAM usage flag
    * Bus conflicts existence flag
    
    Reference: https://wiki.nesdev.com/w/index.php/INES

--- src/rom/model.rs
+++ src/rom/model.rs
@@ -1,16 +1,26 @@
 /// Basic structure used to define what sort of data we should expect to pull from the ROM.
 #[derive(PartialEq, Debug, Clone)]
-pub struct NESHeader {
+pub struct INESHeader {
     // The size of PRG (Program) ROM.
     pub prg_rom_size: usize,
     // The size of CHR (Character) ROM.
     pub chr_rom_size: usize,
     // The nametable mirroring mode.
     pub nametable_mirror_mode: NametableMirroringMode,
+    // A flag denoting if a 512-byte trainer exists at $7000-$71FF.
+    pub trainer_exists: bool,
+    // The mapper number.
+    pub mapper: u8,
     // The system type.
     pub system_type: SystemType,
     // The size of PRG (Program) RAM.
     pub prg_ram_size: usize,
+    // The TV typs.
+    pub tv_type: TVType,
+    // A flag denoting if PRG RAM is used. True represents that the PRG RAM is used.
+    pub prg_ram_used: bool,
+    // A flag denoting if Bus conflicts are present on the board.
+    pub bus_conflicts_present: bool,
 }
 
 /// Definition of possible nametable mirror modes.
@@ -31,7 +41,16 @@ pub enum NametableMirroringMode {
 #[derive(PartialEq, Debug, Clone)]
 pub enum SystemType {
     NES,
-    NES2,
     VSUnisystem,
     PlayChoice10,
+}
+
+/// Definition of possible TV types.
+/// 
+/// Reference: https://wiki.nesdev.com/w/index.php/INES#Flags_9
+#[derive(PartialEq, Debug, Clone)]
+pub enum TVType {
+    NTSC,
+    PAL,
+    DualCompatible,
 }
--- src/rom/reader.rs
+++ src/rom/reader.rs
@@ -7,11 +7,12 @@ use nom::{
     },
     bytes::complete::tag,
     combinator::map_opt,
-    number::complete::{
-        be_u32,
-        be_u8,
-    },
-    sequence::pair,
+    multi::count,
+    number::complete::be_u8,
+    sequence::{
+        pair,
+        tuple,
+    }
 };
 
 pub const KILOBYTE_SIZE: usize = 1024;
@@ -44,20 +45,60 @@ bitflags! {
     struct Flags7: u8 {
         const VS_UNISYSTEM = 1 as u8;
         const PLAYCHOICE_10 = (1 << 1) as u8;
-        const NES_2_0 = (2 << 2) as u8; // 00001000
     }
 }
 
 impl Into<SystemType> for Flags7 {
     fn into(self) -> SystemType {
-        if self.contains(Flags7::NES_2_0) {
-            SystemType::NES2
-        } else if self.contains(Flags7::VS_UNISYSTEM) {
+        if self.contains(Flags7::VS_UNISYSTEM) {
             SystemType::VSUnisystem
         } else if self.contains(Flags7::PLAYCHOICE_10) {
             SystemType::PlayChoice10
         } else {
             SystemType::NES
+        }
+    }
+}
+
+bitflags! {
+    struct Flags9: u8 {
+        const NTSC = 0 as u8;
+        const PAL = 1 as u8;
+    }
+}
+
+impl Into<TVType> for Flags9 {
+    fn into(self) -> TVType {
+        if self.contains(Flags9::PAL) {
+            TVType::PAL
+        } else {
+            TVType::NTSC
+        }
+    }
+}
+
+bitflags! {
+    struct Flags10: u8 {
+        const NTSC = 0 as u8;
+        // Note that technically Dual-Compatible is represented in byte 10 of the header as
+        // *either* 1 or 3. As far as bitflags is concerned though, contains(1) + contains(2)
+        // means we would contains(3).
+        const DUAL_COMPATIBLE = 1 as u8;
+        const PAL = (1 << 1) as u8;
+    }
+}
+
+impl Into<TVType> for Flags10 {
+    fn into(self) -> TVType {
+        // Check for dual-compatible first, because 3 technically contains 2.
+        if self.contains(Flags10::DUAL_COMPATIBLE) {
+            TVType::DualCompatible
+        // Now implied that bit 0 is 0, which means it's safe to check for PAL.
+        } else if self.contains(Flags10::PAL) {
+            TVType::PAL
+        // Now implied that bit 1 is 0, which means it must be NTSC.
+        } else {
+            TVType::NTSC
         }
     }
 }
@@ -69,28 +110,57 @@ fn parse_flag_bits<T>(input: &[u8], from_bits_fn: fn(u8) -> Option<T>) -> IResul
     ))(input)
 }
 
-fn parse_header(input: &[u8]) -> IResult<&[u8], NESHeader> {
+fn parse_flags_10(input: &[u8]) -> IResult<&[u8], (u8, u8, u8, u8, Flags10)> {
+    bits(tuple((
+        take::<_, _, _, (_, _)>(2usize),
+        take::<_, _, _, (_, _)>(1usize), // Does the board have bus conflicts? (0: no, 1: yes)
+        take::<_, _, _, (_, _)>(1usize), // Is PRG RAM used? (0: present, 1: not)
+        take::<_, _, _, (_, _)>(2usize),
+        map_opt(take::<_, _, _, (_, _)>(2usize), Flags10::from_bits)
+    )))(input)
+}
+
+fn parse_ines_header(input: &[u8]) -> IResult<&[u8], INESHeader> {
     let (i, _) = tag(b"NES\x1A")(input)?;
     let (i, prg_page_count) = be_u8(i)?;
     let (i, chr_page_count) = be_u8(i)?;
-    let (i, (_mapper_lo, flags6)) = parse_flag_bits(i, Flags6::from_bits)?;
-    let (i, (_mapper_hi, flags7)) = parse_flag_bits(i, Flags7::from_bits)?;
+    let (i, (mapper_lo, flags6)) = parse_flag_bits(i, Flags6::from_bits)?;
+    let (i, (mapper_hi, flags7)) = parse_flag_bits(i, Flags7::from_bits)?;
     let (i, prg_ram_size) = be_u8(i)?;
-    let (i, _) = be_u8(i)?; // skip byte 9; not in the official specification (TODO)
-    let (i, _) = be_u8(i)?; // skip byte 10; not in the official specification (TODO)
-    let (i, _) = be_u8(i)?; // skip byte 11; unused padding
-    let (i, _) = be_u32(i)?; // skip bytes 12-15; unused padding
+    let (i, (_, flags9)) = parse_flag_bits(i, Flags9::from_bits)?; // Only bit 0 matters
+    let (i, (_, bus_conflicts_f10, prg_ram_used_f10, _, flags10)) = parse_flags_10(i)?;
 
-    Ok((i, NESHeader {
+    // Chomp unused padding bytes 11 - 15. Note that this implicitly destroys compatibility
+    // with the archaic iNES format (which should be fine). In that format, bytes 7-15 would
+    // often be filled with signature strings from an image conversion or auditing tool.
+    // The prior parsing of bytes 7-10 up to this point destroys that compatibility, anyways.
+    // So I'm not going to fret about it.
+    let (i, _) = count(tag(b"\x00"), 5)(i)?;
+
+    let mapper = (mapper_hi << 4) | mapper_lo;
+    let tv_type = if TVType::DualCompatible == flags10.into() {
+        flags10.into()
+    } else {
+        flags9.into()
+    };
+
+    Ok((i, INESHeader {
         prg_rom_size: prg_page_count as usize,
         chr_rom_size: chr_page_count as usize,
         nametable_mirror_mode: flags6.into(),
+        trainer_exists: flags6.contains(Flags6::TRAINER_EXISTS),
+        mapper: mapper,
         system_type: flags7.into(),
         prg_ram_size: if prg_ram_size > 0 {
             prg_ram_size as usize
         } else { // PRG RAM Size specified as 0 is treated as 1 page for compatibility
             1
         },
+        tv_type: tv_type,
+        // Note that we check prg_ram_used_f10 == 0.
+        // For some reason, spec says 0 = used, 1 = not used.
+        prg_ram_used: flags6.contains(Flags6::BATTERY_BACKED_RAM) || prg_ram_used_f10 == 0,
+        bus_conflicts_present: bus_conflicts_f10 != 0,
     }))
 }
 
@@ -107,13 +177,13 @@ mod tests {
             0x45, // E
             0x53, // S
             0x1A, // <EOF>
-            0x02, // PRG ROM has 2 pages
-            0x01, // CHR ROM has 1 page
-            0x31, // Flags6 -> first 4 bits are low nybble of mapper, last 4 map to mirroring mode
-            0x00, // Flags7 -> first 4 bits are high nybble of mapper, last 4 map to system type
-            0x00, // Flags8 -> 8-bit integer specifying the number of pages in PRG RAM
-            0x00, // Flags9 -> lowest bit specifies the TV system (NTSC or PAL)
-            0x00, // Flags10 -> bits 1+2 specify TV system (again), bit 4 specifies if PRG RAM present
+            0x02, // Number of pages in PRG ROM (8-bit integer)
+            0x01, // Number of pages in CHR ROM (8-bit integer)
+            0b00110001, // Mirroring (0+3), PRG RAM flag (1), trainer flag (2), Mapper low (4..7)
+            0b00000000, // System type (0..4), Mapper high (4..7)
+            0x00, // Number of pages in PRG RAM (8-bit integer)
+            0b00000001, // TV system (0)
+            0b00100001, // TV system (0+1), PRG RAM flag (4), board bus conflicts (5)
             0x00, // unused padding
             0x00, // unused padding
             0x00, // unused padding
@@ -121,15 +191,20 @@ mod tests {
             0x00, // unused padding
         ];
 
-        let rom = match parse_header(&data[0..16]) {
+        let rom = match parse_ines_header(&data[0..16]) {
             IResult::Ok((_, rom)) => rom,
             _ => panic!("Failed to parse!"),
         };
 
         expect!(rom.prg_rom_size).to(be_eq(2));
         expect!(rom.chr_rom_size).to(be_eq(1));
-        expect!(rom.prg_ram_size).to(be_eq(1));
         expect!(rom.nametable_mirror_mode).to(be_eq(NametableMirroringMode::Vertical));
+        expect!(rom.trainer_exists).to(be_false());
+        expect!(rom.mapper).to(be_eq(0b00000011));
         expect!(rom.system_type).to(be_eq(SystemType::NES));
+        expect!(rom.prg_ram_size).to(be_eq(1));
+        expect!(rom.tv_type).to(be_eq(TVType::DualCompatible));
+        expect!(rom.prg_ram_used).to(be_true());
+        expect!(rom.bus_conflicts_present).to(be_true());
     }
 }
